e2b9da9f10ddc5bbd6020eb964ac67a020116980
chilusoft <mukukachilu@gmail.com>
2022-02-01 20:55:42 +0200

    initial commit

---
+++ frontend/scripts/index.js
@@ -0,0 +1,16 @@
+// making a fetch request to the server
+
+let url = 'https://61f976ec69307000176f7289.mockapi.io/api/v3/';
+
+
+
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+;
+
+
+
+
+
